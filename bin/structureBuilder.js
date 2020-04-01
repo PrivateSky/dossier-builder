54893d32479c4ef83c0191ce5a810306ab2cdbcb
Cosmin Ursache <asac.cool@gmail.com>
2020-04-01 19:17:53 +0300

    structureBuilder script commit

---
+++ bin/structureBuilder.js
@@ -0,0 +1,35 @@
+let updater = require("updater");
+const config = {
+	workDir: '.',
+	dependencies: [
+		{
+			"name": "foundation_structure",
+			"src": "",
+			"actions":[
+				{
+					"type": "mkdir",
+					"target": "cardinal_dist"
+				},
+				{
+					"type": "mkdir",
+					"target": "psk-release"
+				},
+				{
+					"type": "mkdir",
+					"target": "domain"
+				},
+				{
+					"type": "mkdir",
+					"target": "cardinal_dist"
+				}
+			]
+		}
+	]
+};
+
+updater.setTag("[StructureBuilder]");
+updater.run(config, function(err){
+	if(err){
+		throw err;
+	}
+});
